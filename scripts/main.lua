197fb41f2db041885119b4f6836604ce5a6ee6ee
Rodrigo Delduca <rodrigo@delduca.org>
2025-06-18 09:40:58 -0300

    Work in progress

--- scripts/main.lua
+++ scripts/main.lua
@@ -3,7 +3,7 @@ _G.engine = EngineFactory.new()
   :with_width(1920)
   :with_height(1080)
   :with_scale(4.0)
-  :with_fullscreen(false)
+  :with_fullscreen(true)
   :create()
 
 local objectmanager = engine:objectmanager()
@@ -15,16 +15,14 @@ function setup()
     print("JIT is not available")
   end
 
-  local matrix = objectmanager:create("square")
+  local random = math.random
 
-  math.randomseed(os.time())
-
-  for _ = 1, 50000 do
-    local clone = objectmanager:clone(matrix)
-    clone.action = "default"
-    clone.placement = {
-      x = math.random(0, 480),
-      y = math.random(0, 270)
+  for _ = 1, 60000 do
+    local object = objectmanager:create("square")
+    object.action = "default"
+    object.placement = {
+      x = random(0, 480),
+      y = random(0, 270)
     }
   end
 end
