78cca7ddfb3cd94bb717ce7d1297629fc5bc64a1
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-07 14:01:35 -0300

    Work in progress

--- scenes/benchmark.lua
+++ scenes/benchmark.lua
@@ -5,7 +5,7 @@ local random = math.random
 function scene.on_enter()
   local matrix = scene:get("square", SceneKind.object)
 
-  for _ = 1, 4300 do
+  for _ = 1, 100000 do
     local clone = matrix:clone()
       clone.position = {
         x = random(0, 1920),
