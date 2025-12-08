1be98a075c3a235db94693b277c6809e7a80c40e
Rodrigo Delduca <rodrigo@delduca.org>
2025-12-08 12:47:12 -0300

    Work in progress

--- scenes/benchmark.lua
+++ scenes/benchmark.lua
@@ -5,12 +5,20 @@ local random = math.random
 function scene.on_enter()
   local matrix = scene:get("square", SceneKind.object)
 
-  for _ = 1, 100000 do
+  for _ = 1, 30000 do
     local clone = matrix:clone()
       clone.position = {
         x = random(0, 1920),
         y = random(0, 1080)
       }
+
+      clone:on_hover(function (self)
+        self.angle = 45
+      end)
+
+      clone:on_unhover(function (self)
+        self.angle = 0
+      end)
   end
 end
 
