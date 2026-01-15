ecd509a95f55e28704b4aee77122834081546dab
Rodrigo Delduca <rodrigo@delduca.org>
2026-01-15 19:26:40 -0300

    Work in progress

---
+++ objects/benchmark/square.lua
@@ -0,0 +1,9 @@
+return {
+  on_hover = function()
+    self.angle = 45
+  end,
+
+  on_unhover = function()
+    self.angle = 0
+  end
+}
--- scenes/benchmark.lua
+++ scenes/benchmark.lua
@@ -3,22 +3,14 @@ local scene = {}
 local random = math.random
 
 function scene.on_enter()
-  local matrix = scene:get("square", SceneKind.object)
+  local square = pool.square
 
-  for _ = 1, 30000 do
-    local clone = matrix:clone()
-      clone.position = {
-        x = random(0, 1920),
-        y = random(0, 1080)
-      }
-
-      clone:on_hover(function (self)
-        self.angle = 45
-      end)
-
-      clone:on_unhover(function (self)
-        self.angle = 0
-      end)
+  for _ = 1, 10000 do
+    local clone = square:clone()
+    clone.position = {
+      x = random(0, 1920),
+      y = random(0, 1080)
+    }
   end
 end
 
